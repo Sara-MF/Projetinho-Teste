30cac2997aa66ed3d060e3852126d9b02aaccb3a
saramelo <sara.melo@ipmfor.ce.gov.br>
2022-03-31 16:31:14 -0300

    controller e route criados

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Controllers
+use App\Http\Controllers\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::group(['namespace' => 'Site'], function () {
+//     Route::get('/', HomeController::class);
+// });
+
+Route::get('/', [HomeController::class, '__invoke']);
