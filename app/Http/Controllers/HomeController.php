68816dd703be2122f64217b26280971fb7224713
saramelo <sara.melo@ipmfor.ce.gov.br>
2022-03-31 16:59:00 -0300

    views atualizadas

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,6 +14,6 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return view('site.home.index');
+        return view('site.home');
     }
 }
