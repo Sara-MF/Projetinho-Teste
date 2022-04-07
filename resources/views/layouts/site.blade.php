2acec39010d370d3d2a72bb4075a6c75e04daeb1
saramelo <sara.melo@ipmfor.ce.gov.br>
2022-04-07 15:49:29 -0300

    ajeitando script

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -192,11 +192,11 @@
 
 <!-- Scripts -->
 {{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> --}}
-<script type="teste"></script>
+
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"
         integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 
-<script type="text/javascript" src="lightbox/js/lightbox.js"></script>
+<script type="text/javascript" src="{{ asset('lightbox/js/lightbox.js') }}"></script>
 
 
 <script>
